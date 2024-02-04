10b5a0b954bf036b576834a5905eff71687d2016
최지우 <andyandy0409@naver.com>
2024-02-05 00:07:47 +0900

    20240204save

--- study/20240131/1421.py
+++ study/20240131/1421.py
@@ -1,30 +1,59 @@
-N, C, W = map(int, input().split())
+# N, C, W = map(int, input().split())
 
-trees = []
-for _ in range(N):
-    trees.append(int(input()))
+# trees = []
+# for _ in range(N):
+#     trees.append(int(input()))
 
-result = 0
-length = 10000
-while length > 0:
-    cut = 0
-    cnt = 0
-    for tree in trees:
-        if tree < length:
-            continue
+# result = 0
+# length = 10000
+# while length > 0:
+#     cut = 0
+#     cnt = 0
+#     for tree in trees:
+#         if tree < length:
+#             continue
 
-        tmp = tree//length
-        tmp_cnt = tmp_cut = tmp
+#         tmp = tree//length
+#         tmp_cnt = tmp_cut = tmp
 
-        if tree%length == 0:
-            tmp_cut -= 1
+#         if tree%length == 0:
+#             tmp_cut -= 1
         
-        if tmp_cut*C < tmp_cnt*W*length:
-            cnt += tmp_cnt
-            cut += tmp_cut
+#         if tmp_cut*C < tmp_cnt*W*length:
+#             cnt += tmp_cnt
+#             cut += tmp_cut
 
-    money = W * length * cnt - C * cut
-    result = max(result, money)
-    length -= 1
+#     money = W * length * cnt - C * cut
+#     result = max(result, money)
+#     length -= 1
 
-print(result)
+# print(result)
+
+N, C, W = map(int,input().split())
+woods   = [int(input()) for _ in range(N)]
+
+profit = [] # 각 이득
+
+for i in range(1,max(woods)+1): # 젤 큰 나무까지 순회하면서 자연수 단위로 잘라봄
+    cart = [] # 자른거 담을 바구니
+    cut  = 0
+    total = 0
+    for wood in woods: # 나무 하나씩 꺼내서 잘라봄
+        cutting_wood = [i] * (wood // i)  # 잘라진 나무 더미
+        cart.append(cutting_wood) # 카트로 담아봄
+        
+    print(cart, i, '----', sep='\n')
+    for j in range(len(cart)):
+        if len(cart[j]) > 0: # 일단 최소 한덩이는 나왓음
+            if woods[j] % i == 0:
+                cutting = len(cart[j]) - 1 # 이렇게 만들려고 몇번 잘랐니
+                mymoney = sum(cart[j]) * W - (cutting * C) # 그럼 이 바구니 가격 나옴
+                total += mymoney
+            elif woods[j] % i != 0:
+                cutting = len(cart[j])
+                mymoney = sum(cart[j]) * W - (cutting * C) # 그럼 이 바구니 가격 나옴
+                total += mymoney
+    profit.append(total)
+
+print(profit)
+print(max(profit))
--- swea/01_List_I/Baby_gin/baby_gin.py
+++ swea/01_List_I/Baby_gin/baby_gin.py
@@ -1,22 +1,25 @@
-card_lst = list(map(int, input()))
+T = int(input())
 
-card_cnt = {i: 0 for i in range(0, 12)}
+for tc in range(1, T+1):
+    card_lst = list(map(int, input()))
 
-for card in card_lst:
-    card_cnt[card] += 1
+    card_cnt = {i: 0 for i in range(0, 12)}
 
-tri, run = 0, 0
+    for card in card_lst:
+        card_cnt[card] += 1
 
-for num in range(0, 10):
-    while card_cnt[num] >= 3:
-        card_cnt[num] -= 3
-        tri += 1
-    while card_cnt[num] == 1 and card_cnt[num+1] == 1 and card_cnt[num+2] == 1:
-        for i in range(3):
-            card_cnt[num+1] -= 1
-        run += 1
+    tri, r = 0, 0
 
-if tri+run == 2: print('Baby-gin')
-else: print('lose')
+    for num in range(0, 10):
+        while card_cnt[num] >= 3:
+            card_cnt[num] -= 3
+            tri += 1
+        while card_cnt[num] >= 1 and card_cnt[num+1] >= 1 and card_cnt[num+2] >= 1:
+            for i in range(3):
+                card_cnt[num+i] -= 1
+            r += 1
+
+    if tri+r == 2: print(f'#{tc} true')
+    else: print(f'#{tc} false')
 
 
