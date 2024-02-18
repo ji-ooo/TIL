f8eae134f995b3af340f1d4d38a4c16beb9c485d
최지우 <andyandy0409@naver.com>
2024-02-18 13:33:35 +0900

    20240218save

--- IM/BOJ/10157_자리배정/sol.py
+++ IM/BOJ/10157_자리배정/sol.py
@@ -4,49 +4,52 @@ sys.stdin = open('input.txt')
 C, R = map(int, input().split())
 K = int(input())
 
-cnt = 0
-while True:
-    if R == 1 or C == 1:
-        break
-    else:
-        K -= 2*R + 2*C - 4
-        R -= 2
-        C -= 2
-        cnt += 1
-
-print(K, R, C, cnt)
-arr = [[0] * C for _ in range(R)]
-
 directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
-now = (0, 0)
-d = 0
-if K == 1:
-    x, y = 1, 1
+cnt = 0
+if K > R*C:
+    print(0)
+elif K == 1:
+    print(1, 1)
 else:
-    while K > 0:
+    while K > (R * 2 + C * 2) - 4:
+        if R > 2 and C > 2:
+            K -= (R * 2 + C * 2) - 4
+            R -= 2
+            C -= 2
+            cnt += 1
+    arr = [[-1] * C for _ in range(R)]
+
+    now = (0, 0)
+    d = 0
+    arr[0][0] = K
+    nx, ny = 0, 0
+    while K > 1:
         x, y = now
-        arr[x][y] = K
-        
         dx, dy = directions[d]
         nx, ny = x+dx, y+dy
 
-        if 0 <= nx < R and 0 <= ny < C and arr[nx][ny] == 0:
-            arr[nx][ny] = K
+        if 0 <= nx < R and 0 <= ny < C and arr[nx][ny] == -1:
             K -= 1
             now = (nx, ny)
+            arr[nx][ny] = K
         else:
             d += 1
             if d == 4:
                 d = 0
-    print(y+1, x+1)
+        
+    nx += 1 + cnt
+    ny += 1 + cnt
+    
+    print(ny, nx)
+
+
 '''
-100
-99
-99
-98
-98
-97
-97
-96
+R
+C-1
+R-1
+C-2
+R-2
+C-3
+R-3
 '''
