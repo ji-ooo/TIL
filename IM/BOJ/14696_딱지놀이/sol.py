3ec8cdb876861f613ae3bfe26075fbcdbd105530
최지우 <andyandy0409@naver.com>
2024-02-22 12:49:35 +0900

    20240222lunch

--- IM/BOJ/14696_딱지놀이/sol.py
+++ IM/BOJ/14696_딱지놀이/sol.py
@@ -5,5 +5,25 @@ N = int(input())
 for _ in range(N):
     a, *A = map(int, input().split())
     a, *B = map(int, input().split())
-    print(A, B)
-    # for c in range(1, 5):
+    for c in range(4, 0, -1):
+        a = b = 0
+        for i in A:
+            if i == c:
+                a += 1
+
+        for i in B:
+            if i == c:
+                b += 1
+
+        if a == b:
+            continue
+
+        elif a > b:
+            result = 'A'
+            break
+        else:
+            result = 'B'
+            break
+    else:
+        result = 'D'
+    print(result)
