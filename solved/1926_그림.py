2740fa4a39deedf388be30080a7eeea8449ba766
최지우 <andyandy0409@naver.com>
2024-03-14 17:32:12 +0900

    20240314save

--- solved/1926_그림.py
+++ solved/1926_그림.py
@@ -7,16 +7,25 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 visited = [[0]*m for _ in range(n)]
 
 directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+mx_area = 0
+cnt = 0
 for i in range(n):
     for j in range(m):
-        if arr[i][j]:
+        if arr[i][j] and not visited[i][j]:
+            cnt += 1
             que = deque([(i, j)])
             visited[i][j] = 1
+            area = 0
             while que:
                 x, y = que.pop()
+                area += 1
                 for dx, dy in directions:
                     nx, ny = x + dx, y + dy
                     if 0 <= nx < n and 0 <= ny < m:
                         if arr[nx][ny] and not visited[nx][ny]:
                             que.append((nx, ny))
                             visited[nx][ny] = 1
+            
+            mx_area = max(area, mx_area)
+print(cnt)
+print(mx_area)
