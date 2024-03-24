319e9b6baa01b6c36e83cd5e60bd17f94cae8ec2
최지우 <andyandy0409@naver.com>
2024-03-24 23:33:57 +0900

    20240324save

--- swea2/01_start/5185_이진수/5185.py
+++ swea2/01_start/5185_이진수/5185.py
@@ -4,7 +4,10 @@ sys.stdin = open('5185.txt')
 
 for tc in range(1, int(input()) + 1):
     N, n = map(str, input().split())
-    n = format(int(n, 16), 'b')
-    while len(n)%4 != 0:
-        n = '0' + n
-    print(f'#{tc} {n}')
+    # n = format(int(n, 16), '04b')
+    # while len(n)%4 != 0:
+    #     n = '0' + n
+    # print(f'#{tc} {n}')
+    for i in n:
+        print(format(int(i, 16), '04b'), end='')
+    print()
