e22d9efc589b5b9d0c656782ac96eba902f103b4
최지우 <andyandy0409@naver.com>
2024-05-16 00:00:51 +0900

    20240516

---
+++ PJT/final_pjt/login_api/accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'accounts'
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('logout', views.logout_view, name='logout'),
+]
