dc843c18522bdef7dddfc968ddf2003a8b73c033
최지우 <156387971+ji-ooo@users.noreply.github.com>
2025-02-20 18:03:51 +0900

    Delete algo directory

--- algo/250110/27959.py
+++
@@ -1,8 +0,0 @@
-N, M = map(int, input().split())
-
-bal = 100 * N
-
-if M <= bal:
-    print('Yes')
-else:
-    print('No')
--- algo/250122/17946.py
+++
@@ -1,5 +0,0 @@
-n = int(input())
-for _ in range(n):
-    k = int(input())
-
-    print(1)
