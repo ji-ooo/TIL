6170089b59088de5f67c32899995f40b0cee5a29
최지우 <andyandy0409@gmail.com>
2025-02-20 18:13:54 +0900

    DEL: PJT directory

--- PJT/05_pjt/finace/trends/forms.py
+++
@@ -1,9 +0,0 @@
-from .models import Keyword,Trend
-from django import forms
-class KeywordForm(forms.ModelForm):
-    name = forms.CharField(
-        label='키워드'
-    )
-    class Meta:
-        model = Keyword
-        fields = ('name',)
